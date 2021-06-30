979d3f086caed36dd2261be3f651757df74b59e5
mahmutmercan <56167628+mahmutmercan@users.noreply.github.com>
2021-06-30 09:16:48 +0300

    Create App

---
+++ 824be49b0a23e4664b6156f82ee8ab81/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  824be49b0a23e4664b6156f82ee8ab81
+//
+//  Created by Mahmut MERCAN on 30.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
