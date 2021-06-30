6d35b6389cf0af6d56cde5b2c1e72cc37083bc66
mahmutmercan <56167628+mahmutmercan@users.noreply.github.com>
2021-06-30 10:09:15 +0300

    playButtonBorder

--- 824be49b0a23e4664b6156f82ee8ab81/Controller/ViewController.swift
+++ 824be49b0a23e4664b6156f82ee8ab81/Controller/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setButton()
+    }
+    func setButton() {
+        playButton.layer.borderWidth = 2
+        playButton.layer.borderColor = UIColor.black.cgColor
     }
 
     @IBAction func playButtonTapped(_ sender: Any) {
