c4237a735ef11cd97c96c93e949ceca04c02a1f9
mahmutmercan <56167628+mahmutmercan@users.noreply.github.com>
2021-06-30 12:46:54 +0300

    SpaceStationModelElement Added

---
+++ 824be49b0a23e4664b6156f82ee8ab81/Models/SpaceStationModel.swift
@@ -0,0 +1,20 @@
+//
+//  SpaceStationModel.swift
+//  824be49b0a23e4664b6156f82ee8ab81
+//
+//  Created by Mahmut MERCAN on 30.06.2021.
+//
+
+import Foundation
+
+// MARK: - SpaceStationModelElement
+struct SpaceStationModelElement: Codable {
+    let name: String
+    let coordinateX: Int
+    let coordinateY: Int
+    let capacity: Int
+    let stock: Int
+    let need: Int
+}
+
+typealias SpaceStationModel = [SpaceStationModelElement]
